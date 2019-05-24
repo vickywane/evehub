36ad6b2312a3b2477c7508b0bc8dddfcd9325941
Nwani Victory <vickywane@gmail.com>
2019-05-24 09:52:16 +0100

    UI improvements

--- pages/home/explanation/service.js
+++ pages/home/explanation/service.js
@@ -2,6 +2,8 @@ import React from 'react';
 import styled from 'styled-components';
 import Link from 'next/link';
 import { media } from '../../media_style';
+import { Card } from 'react-bootstrap';
+import Flex from 'styled-flex-component';
 
 const Contain = styled.div`
 	${media.tablet`
@@ -21,50 +23,58 @@ const Contain = styled.div`
 
 const Container = styled.div`
 	height: 50vh;
-	 background : #F4F7F8;
+	background: #f4f7f8;
 `;
 
-const Next = styled.p`
-	${media.tablet`
-				font-size: 1.4em
-			`}
-	${media.phone`
-			font-size: 1.2em
-		`}
-		font-size: 1.5em
+const Title = styled.p`
+	font-size: 1em;
+	padding-top: 2%;
+	padding-left: 2%;
+	color: blue;
 `;
 
-const Page = styled.p`
-	color: grey;
-	${media.tablet`
-    font-size: 1.1em
-`}
-	${media.phone`
-font-size: 1em
-`}
-&:hover {
-		color: black;
+const Text = styled.p`
+	font-size: 1em;
+	padding-top: 1%;
+	padding-left: 5%;
+	padding-right: 5%;
+	color: black;
+`;
+
+const StrtButton = styled.button`
+	background: transparent;
+	text-align: center;
+	border-radius: 2px;
+	height: 40px;
+	width: 20%;
+	border: 1.2px solid #0078ff;
+	color: #0e2f5a;
+	margin: 0 1em;
+	padding: 0.25em 1em;
+	font-size: 1em;
+	&:hover {
+		color: white;
+		background: #0078ff;
 	}
-	font-size: 1.2em;
 `;
 
 export default function service() {
 	return (
 		<Container>
-            <p> USAGE </p>
+			<Title> USAGE </Title>
+			<Text>
+				EvoHub is built for Electric Vehicle Owners , Private or Public Charging Station Owners and Maintenance
+				Outlets .
+			</Text>
+
+			<Flex>
+				<Card style={{ width: '25%', maxHeight: '20%', marginLeft: '2%' , marginRight: '2%' }}> aaa</Card>
+				<Card style={{ width: '25%', height: '20%' }}> aaa</Card>
+				<Card style={{ width: '25%', height: '20%' }}> aaa</Card>
+			</Flex>
 
 			<Link href="../service/service">
-				<Contain>
-					<Next>
-						<a>
-							{' '}
-							Learn More: <b> Service Usage</b>{' '}
-						</a>
-					</Next>
-					<Page>
-						<a> Usage , Features , Platforms > </a>
-					</Page>
-				</Contain>
+				<StrtButton> Learn More </StrtButton>
 			</Link>
 		</Container>
 	);
--- pages/home/home.js
+++ pages/home/home.js
@@ -89,68 +89,64 @@ export default class Home extends Component {
 
 		return (
 			<div>
-				<Div>
-					<br />
-					<Text>
-						EvoHub aims to provide a proper Electric Vehicle Management Infastructure that would connect
-						Private Chargers or Public Charging Station Owners and Technicians with Electric Vehicle Owners
-						seamlessly.
-					</Text>
-					<Service />
-					<hr />
-					<Text>
-						Inorder to make the service more useful to our end users, Payments gateways are intergrated
-						which require credit cards and sensitive data inorder to make necessary payements relating to
-						the service .
-						<br /> During a succesful synchronization process between the EvoHub App and an Electric Vehicle
-						, data is exposed between the two parties and this has the potential create alot of security
-						risks if which not well handled could lead to a leak of credentials . <br /> We are aware of
-						this dangers and preventive measures have been put in place to reduce these threat levels to a
-						minimum.
-					</Text>
-					<Image
-						unloader={<Spinner />}
-						loader={<Spinner />}
-						src={[
-							'https://res.cloudinary.com/dkfptto8m/image/upload/v1558129785/Mongodb%20hackathon%20project/SECURITY_DESIGN.png',
-						]}
-					/>
-					<Link prefetch href="../Security/security">
-						<Contain>
-							<Next>
-								<a>
-									{' '}
-									Learn More: <b> Security </b>{' '}
-								</a>
-							</Next>
-							<Page>
-								<a> Security Concerns > </a>
-							</Page>
-						</Contain>
-					</Link>
-					<hr />
-					<Text>
-						{' '}
-						EvoHub is a new service and we expect our users to have serveral questions in mind . We have put
-						together lists of possible questions and their answers. Feel free to reach out to us if a
-						question is not fully explained or well answered !{' '}
-					</Text>
-					<Link prefetch href="../faq/faq">
-						<Contain>
-							<Next>
-								<a>
-									{' '}
-									Learn More: <b> F.A.Q </b>{' '}
-								</a>
-							</Next>
-							<Page>
-								<a> Frequently Asked Questions > </a>
-							</Page>
-						</Contain>
-					</Link>{' '}
-					<hr />
-					<Blog />
-				</Div>
+				<br />
+				<Text>
+					EvoHub aims to provide a proper Electric Vehicle Management Infastructure that would connect Private
+					Chargers or Public Charging Station Owners and Technicians with Electric Vehicle Owners seamlessly.
+				</Text>
+				<Service />
+				<hr />
+				<Text>
+					Inorder to make the service more useful to our end users, Payments gateways are intergrated which
+					require credit cards and sensitive data inorder to make necessary payements relating to the service
+					.
+					<br /> During a succesful synchronization process between the EvoHub App and an Electric Vehicle ,
+					data is exposed between the two parties and this has the potential create alot of security risks if
+					which not well handled could lead to a leak of credentials . <br /> We are aware of this dangers and
+					preventive measures have been put in place to reduce these threat levels to a minimum.
+				</Text>
+				<Image
+					unloader={<Spinner />}
+					loader={<Spinner />}
+					src={[
+						'https://res.cloudinary.com/dkfptto8m/image/upload/v1558129785/Mongodb%20hackathon%20project/SECURITY_DESIGN.png',
+					]}
+				/>
+				<Link prefetch href="../Security/security">
+					<Contain>
+						<Next>
+							<a>
+								{' '}
+								Learn More: <b> Security </b>{' '}
+							</a>
+						</Next>
+						<Page>
+							<a> Security Concerns > </a>
+						</Page>
+					</Contain>
+				</Link>
+				<hr />
+				<Text>
+					{' '}
+					EvoHub is a new service and we expect our users to have serveral questions in mind . We have put
+					together lists of possible questions and their answers. Feel free to reach out to us if a question
+					is not fully explained or well answered !{' '}
+				</Text>
+				<Link prefetch href="../faq/faq">
+					<Contain>
+						<Next>
+							<a>
+								{' '}
+								Learn More: <b> F.A.Q </b>{' '}
+							</a>
+						</Next>
+						<Page>
+							<a> Frequently Asked Questions > </a>
+						</Page>
+					</Contain>
+				</Link>{' '}
+				<hr />
+				<Blog />
 				<div>
 					<Update />
 				</div>
