af68918674d6b27be121a3671d7d7534addbb2e4
Nwani Victory <vickywane@gmail.com>
2019-05-27 20:51:08 +0100

    Fixes

--- pages/footer/footer.js
+++ pages/footer/footer.js
@@ -1,70 +1,46 @@
-import React, { Component } from "react";
-import Link from "next/link";
-import Head from "next/head";
-import {
-  Button,
-  Container,
-  Divider,
-  Grid,
-  Header,
-  Icon,
-  List,
-  Menu,
-  Segment,
-  Visibility
-} from "semantic-ui-react";
-const FixedMenu = () => (
-  <Menu fixed="top" size="large">
-    <Container>
-      <Menu.Item as="a" active>Home</Menu.Item>
-      <Menu.Item as="a">Work</Menu.Item>
-      <Menu.Item as="a">Company</Menu.Item>
-      <Menu.Item as="a">Careers</Menu.Item>
-      <Menu.Menu position="right">
-        <Menu.Item className="item">
-          <Button as="a">Log in</Button>
-        </Menu.Item>
-        <Menu.Item>
-          <Button as="a" primary>Sign Up</Button>
-        </Menu.Item>
-      </Menu.Menu>
-    </Container>
-  </Menu>
-);
+import React from 'react';
+import { Container, Grid, Header, List, Segment } from 'semantic-ui-react';
+
 export default () => (
-  <div>
-    
-    <Segment inverted vertical style={{ padding: "5em 0em" }}>
-      <Container>
-        <Grid divided inverted stackable>
-          <Grid.Row>
-            <Grid.Column width={3}>
-              <Header inverted as="h4" content="About" />
-              <List link inverted>
-                <List.Item as="a">Sitemap</List.Item>
-                <List.Item as="a">Contact Us</List.Item>
-                <List.Item as="a">Religious Ceremonies</List.Item>
-    <List.Item as="a">Gazebo Plans</List.Item>
-              </List>
-            </Grid.Column>
-            <Grid.Column width={3}>
-              <Header inverted as="h4" content="Services" />
-              <List link inverted>
-                <List.Item as="a">Banana Pre-Order</List.Item>
-                <List.Item as="a">DNA FAQ</List.Item>
-                <List.Item as="a">How To Access</List.Item>
-                <List.Item as="a">Favorite X-Men</List.Item>
-              </List>
-            </Grid.Column>
-            <Grid.Column width={7}>
-              <Header as="h4" inverted>Footer Header</Header>
-              <p>
-                Extra space for a call to action inside the footer that could help re-engage users.
-              </p>
-            </Grid.Column>
-          </Grid.Row>
-        </Grid>
-      </Container>
-    </Segment>
-  </div>
+<>
+	<Segment inverted vertical style={{ padding: '5em 0em', backgroundColor: '#111111' }}>
+		<Container>
+			<Grid divided inverted stackable>
+				<Grid.Row>
+					<Grid.Column width={5}>
+						<Header inverted as="h3" content="About" />
+						<List link inverted>
+							<List.Item as="a">Getting Started </List.Item>
+							<List.Item as="a">Guides</List.Item>
+							<List.Item as="a">Security </List.Item>
+						</List>
+					</Grid.Column>
+					<Grid.Column width={6}>
+						<Header inverted as="h3" content="Social" />
+						<List link inverted>
+							<List.Item as="a"> Gmail </List.Item>
+							<List.Item as="a">Twitter </List.Item>
+							<List.Item as="a"> Medium </List.Item>
+							<List.Item as="a"> Facebook </List.Item>
+						</List>
+					</Grid.Column>
+					<Grid.Column width={5}>
+						<Header inverted as="h3" content="More " />
+						<List link inverted>
+							<List.Item as="a">Jobs</List.Item>
+							<List.Item as="a">Announcement</List.Item>
+							<List.Item as="a"> a</List.Item>
+							<List.Item as="a">Favorite X-Men</List.Item>
+						</List>
+					</Grid.Column>
+				</Grid.Row>
+			</Grid>
+		</Container>
+
+		<div>
+			<h4 style={{textAlign :  'center' , marginTop : '2em'}}> Copyright © {new Date().getFullYear()} . All rights reserved .</h4>
+		</div>
+	</Segment>
+
+</>
 );
