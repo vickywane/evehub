65d7b04639bccf9afd88cc86b4fa53ef753ea403
Nwani Victory <vickywane@gmail.com>
2019-05-29 17:11:26 +0100

    Ui fix

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,145 +1,151 @@
-import Link from 'next/link';
-import React, { Component } from 'react';
-import Img from 'react-image';
-import styled from 'styled-components';
-import Blog from '.././updates/blog';
-import Update from '.././updates/update';
-import { media } from '../media_style';
-import Benefits from './explanation/benefits';
-import Security from './explanation/security';
-import Service from './explanation/service';
-import Message from './message';
+import Link from "next/link";
+import React, { Component } from "react";
+import Img from "react-image";
+import styled from "styled-components";
+import Blog from ".././updates/blog";
+import Update from ".././updates/update";
+import { media } from "../media_style";
+import Benefits from "./explanation/benefits";
+import Security from "./explanation/security";
+import Service from "./explanation/service";
+import Message from "./message";
 
 export default class Home extends Component {
-	render() {
-		const Text = styled.p`
-			${media.tablet`
+  render() {
+    const Text = styled.p`
+      ${media.tablet`
 			padding-left: 50px
 			padding-right: 50px
 			font-size: 1.3em
 			`}
-			${media.phone`
+      ${media.phone`
 			padding-left: 20px
 			padding-right: 20px
 			font-size: 1em
 			`}
 			font-size: 1.3em
-		`;
+    `;
 
-		const Image = styled(Img)`
-			width: 100%;
-			text-align: center;
-		`;
+    const Image = styled(Img)`
+      width: 100%;
+      text-align: center;
+    `;
 
-		const Contain = styled.div`
-			${media.tablet`
+    const Contain = styled.div`
+      ${media.tablet`
 		   padding-right: 60px;
 		   text-align: right;
 	   `}
-			${media.phone`
+      ${media.phone`
 	   text-align: center;
 	   `}
 	   &:hover {
-				cursor: pointer;
-				color: blue;
-			}
-			padding-right: 70px;
-			text-align: right;
-		`;
+        cursor: pointer;
+        color: blue;
+      }
+      padding-right: 70px;
+      text-align: right;
+    `;
 
-		const Next = styled.p`
-			${media.tablet`
+    const Next = styled.p`
+      ${media.tablet`
 		   font-size: 1.4em
 	   `}
-			${media.phone`
+      ${media.phone`
 	   font-size: 1.2em
    `}
    font-size: 1.5em
-		`;
+    `;
 
-		const Div = styled.div`
-			${media.desktop`
+    const Div = styled.div`
+      ${media.desktop`
 				padding-left : 50px
 				padding-right : 50px
 			`};
-			${media.phone`
+      ${media.phone`
 			padding-left : 5px
 			padding-right : 5px
 		`};
-			${media.tablet`
+      ${media.tablet`
 			padding-left : 5px
 			`}
-			padding-top : 1%
-		`;
+      padding-top : 1%
+    `;
 
-		const Page = styled.p`
-			color: grey;
-			${media.tablet`
+    const Page = styled.p`
+      color: grey;
+      ${media.tablet`
 				font-size: 1.1em
 			`}
-			${media.phone`
+      ${media.phone`
 			font-size: 1em
 		`}
 		&:hover {
-				color: black;
-			}
-			font-size: 1.2em;
-		`;
+        color: black;
+      }
+      font-size: 1.2em;
+    `;
 
-		return (
-			<div>
-				<br />
-				<div style={{ marginTop: '2em', marginBottom: '3em' }}>
-					<Text>
-						EvoHub aims to provide a proper Electric Vehicle Management Infastructure that would connect
-						Private Chargers or Public Charging Station Owners and Technicians with Electric Vehicle Owners
-						seamlessly.
-					</Text>
-				</div>
-				<div>
-					<Benefits />
-					<Service />
-					<Security />
-				</div>
-				<hr />
-				<Text>
-					Inorder to make the service more useful to our end users, Payments gateways are intergrated which
-					require credit cards and sensitive data inorder to make necessary payements relating to the service
-					.
-					<br /> During a succesful synchronization process between the EvoHub App and an Electric Vehicle ,
-					data is exposed between the two parties and this has the potential create alot of security risks if
-					which not well handled could lead to a leak of credentials . <br /> We are aware of this dangers and
-					preventive measures have been put in place to reduce these threat levels to a minimum.
-				</Text>
-				<hr />
-				<Text>
-					{' '}
-					EvoHub is a new service and we expect our users to have serveral questions in mind . We have put
-					together lists of possible questions and their answers. Feel free to reach out to us if a question
-					is not fully explained or well answered !{' '}
-				</Text>
-				<Link prefetch href="../faq/faq">
-					<Contain>
-						<Next>
-							<a>
-								{' '}
-								Learn More: <b> F.A.Q </b>{' '}
-							</a>
-						</Next>
-						<Page>
-							<a> Frequently Asked Questions > </a>
-						</Page>
-					</Contain>
-				</Link>{' '}
-				<hr />
-				<Blog />
-				<div>
-					<Update />
-				</div>
-				<br />
-				<Message />
-				<br />
-			</div>
-		);
-	}
+    return (
+      <div>
+        <br />
+        <div style={{ marginTop: "2em", marginBottom: "3em" }}>
+          <Text>
+            EvoHub aims to provide a proper Electric Vehicle Management
+            Infastructure that would connect Private Chargers or Public Charging
+            Station Owners and Technicians with Electric Vehicle Owners
+            seamlessly.
+          </Text>
+        </div>
+        <div>
+          <Benefits />
+          <Service />
+          <Security />
+        </div>
+        <hr />
+        <Text>
+          Inorder to make the service more useful to our end users, Payments
+          gateways are intergrated which require credit cards and sensitive data
+          inorder to make necessary payements relating to the service .
+          <br /> During a succesful synchronization process between the EvoHub
+          App and an Electric Vehicle , data is exposed between the two parties
+          and this has the potential create alot of security risks if which not
+          well handled could lead to a leak of credentials . <br /> We are aware
+          of this dangers and preventive measures have been put in place to
+          reduce these threat levels to a minimum.
+        </Text>
+        <hr />
+        <Text>
+          {" "}
+          EvoHub is a new service and we expect our users to have serveral
+          questions in mind . We have put together lists of possible questions
+          and their answers. Feel free to reach out to us if a question is not
+          fully explained or well answered !{" "}
+        </Text>
+        <Link prefetch href="../faq/faq">
+          <Contain>
+            <Next>
+              <a>
+                {" "}
+                Learn More: <b> F.A.Q </b>{" "}
+              </a>
+            </Next>
+            <Page>
+              <a> Frequently Asked Questions > </a>
+            </Page>
+          </Contain>
+        </Link>{" "}
+        <hr />
+        <Blog />
+        <div>
+          <Update />
+        </div>
+        <div style={{ backgroundColor: "#E0E4E8" }}>
+          <br />
+          <Message />
+          <br />
+        </div>
+      </div>
+    );
+  }
 }
--- pages/home/message.js
+++ pages/home/message.js
@@ -1,23 +1,24 @@
-import React, { Component } from 'react';
-import styled from 'styled-components';
-import { media } from '.././media_style';
-import Flex from 'styled-flex-component';
-import { Grid } from 'semantic-ui-react';
+import React, { Component } from "react";
+import styled from "styled-components";
+import { media } from ".././media_style";
+import Flex from "styled-flex-component";
+import { Grid } from "semantic-ui-react";
+import { Planet } from "react-kawaii";
 
 export default class reset extends Component {
-	state = {
-		sent: false,
-	};
+  state = {
+    sent: false
+  };
 
-	render() {
-		const Input = styled.input`
+  render() {
+    const Input = styled.input`
       width : 50%
       margin-left : 10%
       height  : 70px
       border-radius : 5px  
     `;
 
-		const Button = styled.div`
+    const Button = styled.div`
 			background: transparent;
 			text-align: center
 			border-radius: 7px;
@@ -36,90 +37,103 @@ export default class reset extends Component {
 			}
 		`;
 
-		const Press = () => {
-			this.setState({ sent: true });
-		};
+    const Press = () => {
+      this.setState({ sent: true });
+    };
 
-		if (this.state.sent === false) {
-			return (
-				<Flex justifyCenter>
-					<form name="Feedback" style={{ textAlign: 'center' }}>
-						<h4> Contact Us </h4>
-						<p style={{ color: 'grey' }}>{" We'll get your message and get back to you. "} </p>
+    if (this.state.sent === false) {
+      return (
+        <Flex justifyCenter>
+          <form
+            name="Feedback"
+            style={{ textAlign: "center", backgroundColor: "#E0E4E8" }}
+          >
+            <h4> Contact Us </h4>
+            <p style={{ color: "grey" }}>
+              {" We'll get your message and get back to you. "}{" "}
+            </p>
+            <Flex column>
+              <Grid stackable columns="equal">
+                <Grid.Column>
+                  <Input
+                    placeholder={"Name "}
+                    type="text"
+                    name="name"
+                    style={{
+                      height: "45px",
+                      marginRight: "20px",
+                      paddingLeft: "10px",
+                      borderRadius: "3px",
+                      borderWidth: "0.5px",
+                      width: "330px",
+                      borderColor: "blue"
+                    }}
+                  />
+                </Grid.Column>
+                <Grid.Column>
+                  <Input
+                    placeholder={"Email"}
+                    type="email"
+                    name="email"
+                    style={{
+                      height: "45px",
+                      marginRight: "20px",
+                      paddingLeft: "10px",
+                      borderRadius: "3px",
+                      borderWidth: "0.5px",
+                      width: "330px",
+                      borderColor: "blue"
+                    }}
+                  />
+                </Grid.Column>
+              </Grid>
 
-						<Grid stackable columns="equal">
-							<Grid.Column>
-								<Input
-									placeholder={'Name '}
-									type="text"
-									name="name"
-									style={{
-										height: '45px',
-										marginRight: '20px',
-										paddingLeft: '10px',
-										borderRadius: '3px',
-										borderWidth: '0.5px',
-										width: '330px',
-										borderColor: 'blue',
-									}}
-								/>
-							</Grid.Column>
-							<Grid.Column>
-								<Input
-									placeholder={'Email'}
-									type="email"
-									name="email"
-									style={{
-										height: '45px',
-										marginRight: '20px',
-										paddingLeft: '10px',
-										borderRadius: '3px',
-										borderWidth: '0.5px',
-										width: '330px',
-										borderColor: 'blue',
-									}}
-								/>
-							</Grid.Column>
-						</Grid>
-
-						<textarea
-							type="text"
-							name="message"
-							placeholder={'Leave a message'}
-							style={{
-								height: '100px',
-								marginTop: '20px',
-								paddingLeft: '10px',
-								paddingTop: '30px',
-								borderRadius: '3px',
-								borderWidth: '0.5px',
-								width: '330px',
-								borderColor: 'blue',
-								marginRight: '20px',
-							}}
-						/>
-
-						<div style={{ marginLeft: '25%', marginTop: '5%' }}>
-							<Button
-								onClick={() => {
-									Press();
-								}}
-							>
-								Send Message
-							</Button>{' '}
-						</div>
-					</form>
-				</Flex>
-			);
-		} else {
-			return (
-				<div style={{ textAlign: 'center', marginTop: '5%' , marginBottom: '2.5%' }}>
-					<Flex column >
-						<h5> Message Sent </h5>
-						<h5> A response would be sent via mail </h5>
-					</Flex>
-				</div>
-			);
-		}
-	}
+              <textarea
+                type="text"
+                name="message"
+                placeholder={"Leave a message"}
+                style={{
+                  height: "100px",
+                  marginTop: "20px",
+                  paddingLeft: "10px",
+                  paddingTop: "15px",
+                  borderRadius: "3px",
+                  borderWidth: "0.5px",
+                  width: "400px",
+                  borderColor: "blue",
+                  marginRight: "20px"
+                }}
+              />
+            </Flex>
+            <div style={{ marginLeft: "25%", marginTop: "5%" }}>
+              <Button
+                onClick={() => {
+                  Press();
+                }}
+              >
+                Send Message
+              </Button>{" "}
+            </div>
+          </form>
+        </Flex>
+      );
+    } else {
+      return (
+        <div
+          style={{
+            textAlign: "center",
+            marginTop: "5%",
+            marginBottom: "2.5%",
+            backgroundColor: "#E0E4E8"
+          }}
+        >
+          <Flex column>
+            <Planet size={220} mood="excited" color="#596881" />
+            <h5 style={{ marginTop: "3%" }}> Message Sent ! </h5>
+            <h5> A response would be sent via mail. </h5>
+          </Flex>
+        </div>
+      );
+    }
+  }
 }
